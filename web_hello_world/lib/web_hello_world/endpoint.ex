e2edc3840b22185631a44c182721b81e98038f2f
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-02-02 22:31:44 +0100

    Remove static serving

--- web_hello_world/lib/web_hello_world/endpoint.ex
+++ web_hello_world/lib/web_hello_world/endpoint.ex
@@ -1,15 +1,6 @@
 defmodule WebHelloWorld.Endpoint do
   use Phoenix.Endpoint, otp_app: :web_hello_world
 
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phoenix.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/", from: :web_hello_world, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   plug Plug.RequestId
   plug Plug.Logger
 
