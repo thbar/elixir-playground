d9646b8efaf387340594199d74314b2beebc3d12
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-02-02 22:36:53 +0100

    Remove html pipeline

--- web_hello_world/web/router.ex
+++ web_hello_world/web/router.ex
@@ -1,26 +1,13 @@
 defmodule WebHelloWorld.Router do
   use Phoenix.Router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", WebHelloWorld do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", WebHelloWorld do
-  #   pipe_through :api
-  # end
 end
