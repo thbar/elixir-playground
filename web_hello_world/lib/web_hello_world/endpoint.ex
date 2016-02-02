3a857e580f3eb586ba76ac7e41d28aecbfc31556
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-02-02 22:58:47 +0100

    Remove parser for now

--- web_hello_world/lib/web_hello_world/endpoint.ex
+++ web_hello_world/lib/web_hello_world/endpoint.ex
@@ -1,10 +1,5 @@
 defmodule WebHelloWorld.Endpoint do
   use Phoenix.Endpoint, otp_app: :web_hello_world
 
-  plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Poison
-
   plug WebHelloWorld.Router
 end
