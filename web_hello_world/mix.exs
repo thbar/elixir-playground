f0d371d9d8be9d170ffe82f202570d4ea45eecff
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-02-02 23:17:38 +0100

    Remove more code

--- web_hello_world/mix.exs
+++ web_hello_world/mix.exs
@@ -5,30 +5,19 @@ defmodule MyMixfile do
     [app: :my_application,
      version: "0.0.1",
      elixir: "~> 1.0",
-     elixirc_paths: elixirc_paths(Mix.env),
+     elixirc_paths: ["lib"],
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: [
+       {:phoenix, "~> 1.1.4"},
+       {:cowboy, "~> 1.0"}]
+     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [mod: {MyApplication, []},
     # I tried to remove :logger but I get an exception
      applications: [:phoenix, :cowboy, :logger]]
-  end
-
-  # Specifies which paths to compile
-  defp elixirc_paths(_),     do: ["lib", "web"]
-
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
-  defp deps do
-    [{:phoenix, "~> 1.1.4"},
-     {:cowboy, "~> 1.0"}]
   end
 end
