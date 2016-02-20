389a24400f9a6773c9c51d3b58edbf8cd622acaf
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-02-20 11:33:31 +0100

    Extract method

--- samples/issues/lib/issues/github.ex
+++ samples/issues/lib/issues/github.ex
@@ -1,10 +1,14 @@
 defmodule Issues.GitHub do
   def fetch_issues(user, project) do
-    url = "https://api.github.com/repos/#{user}/#{project}/issues"
+    url = issues_url(user, project)
     # TODO: add user agent, but verify and understand the data structure
     # passed as a parameter first
     response = HTTPoison.get(url)
     handle_response(response)
+  end
+
+  def issues_url(user, project)
+    "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
