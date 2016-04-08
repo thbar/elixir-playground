4cc26a6153186efaa375b9c5250de3f9cdf921ce
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-08 20:14:37 +0200

    Implement parsing
    This is as recommended by the book, yet my feeling is that I would
    probably structure it a bit differently.

--- samples/issues/lib/issues/cli.ex
+++ samples/issues/lib/issues/cli.ex
@@ -21,5 +21,11 @@ defmodule Issues.CLI do
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [help: :boolean],
                                      aliases: [h: :help])
+    case parse do
+      {[help: true], _, _} -> :help
+      {_, [user, project, count], _} -> {user, project, count}
+      {_, [user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
   end
 end
