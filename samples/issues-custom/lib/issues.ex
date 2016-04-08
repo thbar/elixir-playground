4a792194e686fe71822b5957b505dc0f0e6b7b09
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-08 08:37:38 +0200

    Remove issues version which diverged too much from the book

--- samples/issues-custom/lib/issues.ex
+++
@@ -1,2 +0,0 @@
-defmodule Issues do
-end
--- samples/issues-custom/test/test_helper.exs
+++
@@ -1,3 +0,0 @@
-ExUnit.start()
-# trace: true is apparently activating the "documentation" kind of formatter
-ExUnit.configure exclude: :pending, trace: true
