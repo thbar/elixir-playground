e36a3d756dea7faa94b9650a5d3669eff186f6f1
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-08 20:39:34 +0200

    Parse count

--- samples/issues/lib/issues/cli.ex
+++ samples/issues/lib/issues/cli.ex
@@ -23,7 +23,7 @@ defmodule Issues.CLI do
                                      aliases: [h: :help])
     case parse do
       {[help: true], _, _} -> :help
-      {_, [user, project, count], _} -> {user, project, count}
+      {_, [user, project, count], _} -> {user, project, String.to_integer(count)}
       {_, [user, project], _} -> {user, project, @default_count}
       _ -> :help
     end
