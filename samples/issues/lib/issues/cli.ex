5e3d98a9d1cf26b7cff7f5483a99c7702887c7b1
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-11 23:44:15 +0200

    Implement more error processing

--- samples/issues/lib/issues/cli.ex
+++ samples/issues/lib/issues/cli.ex
@@ -21,6 +21,15 @@ defmodule Issues.CLI do
   
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
+    |> decode_response
+  end
+  
+  def decode_response({:ok, body}), do: body
+  
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
   end
 
   @doc """
