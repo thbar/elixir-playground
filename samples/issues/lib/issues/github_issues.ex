4dc5a041667867a108817ea5fb8db77cc0540d77
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-11 23:33:48 +0200

    Implement actual fetching

--- samples/issues/lib/issues/github_issues.ex
+++ samples/issues/lib/issues/github_issues.ex
@@ -6,11 +6,21 @@ defmodule Issues.GithubIssues do
 
   def fetch(user, project) do
     issues_url(user, project)
-      |> HTTPoison.get(@user_agent)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
   end
   
   def issues_url(user, project) do
     "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+
+  # TODO: find a DRYer way to implement this.
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    { :ok, body }
+  end
+  
+  def handle_response({_, %{status_code: _, body: body}}) do
+    { :error, body }
   end
   
 end
