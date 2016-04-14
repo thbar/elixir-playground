84f695591bcb205773407c3ef2c2d322e11f95fa
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-14 17:54:30 +0200

    Add sorting

--- samples/issues/lib/issues/cli.ex
+++ samples/issues/lib/issues/cli.ex
@@ -23,6 +23,12 @@ defmodule Issues.CLI do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
     |> convert_to_list_of_maps
+    |> sort_into_ascending_order
+  end
+  
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
   
   def convert_to_list_of_maps(list) do
--- samples/issues/test/cli_test.exs
+++ samples/issues/test/cli_test.exs
@@ -2,7 +2,11 @@ defmodule CLITest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [
+    parse_args: 1,
+    sort_into_ascending_order: 1,
+    convert_to_list_of_maps: 1
+  ]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
@@ -14,5 +18,17 @@ defmodule CLITest do
   
   test "count is defaulted if two values given" do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+  
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: issue["created_at"]
+    assert issues == ~w{a b c}
+  end
+  
+  defp fake_created_at_list(values) do
+    data = for value <- values,
+      do: [{"created_at", value}, {"other_data", "xxx"} ]
+    convert_to_list_of_maps data
   end
 end
