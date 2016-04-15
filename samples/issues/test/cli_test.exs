109e2103e6d1e79456b5fcaac85c5ad6957c5125
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-16 01:33:04 +0200

    Remove obsolete test

--- samples/issues/test/cli_test.exs
+++ samples/issues/test/cli_test.exs
@@ -26,23 +26,6 @@ defmodule CLITest do
     assert issues == ~w{a b c}
   end
   
-  @tag :pending
-  test "exercice: OrganizingAProject-4" do
-    IO.puts """
-    (Tricky) Before reading the next section, see if you can write the code to
-    format the data into columns, like the sample output at the start of the
-    chapter. This is probably the longest piece of Elixir code you’ll have
-    written. Try to do it without using if or cond.
-    
-     #  | created_at           | title
-    ----+----------------------+-----------------------------------------
-    889 | 2013-03-16T22:03:13Z | MIX_PATH environment variable (of sorts)
-    892 | 2013-03-20T19:22:07Z | Enhanced mix test --cover
-    893 | 2013-03-21T06:23:00Z | mix test time reports
-    898 | 2013-03-23T19:19:08Z | Add mix compile --warnings-as-errors
-    """
-  end
-  
   defp fake_created_at_list(values) do
     data = for value <- values,
       do: [{"created_at", value}, {"other_data", "xxx"} ]
