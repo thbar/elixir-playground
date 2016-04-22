576583e23a8b80557a59ed7f1d2c11403cc5f5fe
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-04-22 21:32:31 +0200

    Extract one more function

--- samples/issues/lib/issues/tableizer.ex
+++ samples/issues/lib/issues/tableizer.ex
@@ -7,24 +7,33 @@ defmodule Tableizer do
     # for pick the right values in the right order
     [fields | Enum.map(list_of_maps, fetcher)]
   end
-  
-  # A first ugly yet working method to format as ascii table
-  def tableize(rows) do
+
+  def stringify(rows) do
     # I cannot call to_string on a list of atoms without an exception
     # so I'm baking a working version of this here
     stringify = fn (list) -> Enum.map(list, &(to_string(&1))) end
     
     # I can now convert fields and values to a list of list of strings
-    rows = Enum.map(rows, stringify)
-    
-    [fields | values] = rows
-    columns = (0..Enum.count(fields)-1)
-    
+    Enum.map(rows, stringify)
+  end
+  
+  def compute_max_width(rows) do
+    [headers | _] = rows
+    columns = (0..Enum.count(headers)-1)
+
     # which allows me to compute the width of each column
     widths = for column <- columns do
       col_values = Enum.map(rows, &(Enum.at(&1, column)))
       Enum.reduce(col_values, 0, fn(x, max) -> Enum.max([String.length(x), max]) end)
     end
+  end
+  
+  # A first ugly yet working method to format as ascii table
+  def tableize(rows) do
+    widths = compute_max_width(rows)
+
+    [fields | values] = rows
+    columns = (0..Enum.count(fields)-1)
         
     # justify each column
     rows = for row <- rows do
--- samples/issues/test/tableizer_test.exs
+++ samples/issues/test/tableizer_test.exs
@@ -14,12 +14,30 @@ defmodule TableizerTest do
       [28, "Mary J Blige"]
     ]
   end
-
+  
+  test "converts everything to strings" do
+    output = stringify([[:this, :that],[50, "test"]])
+    
+    assert output == [
+      ["this", "that"],
+      ["50","test"]
+    ]
+  end
+  
+  test "computes max width per column" do
+    output = stringify([
+      [:foobar, :foo, :bar],
+      [1, 4, "foobar"]
+    ]) |> compute_max_width
+    
+    assert output == [ 6, 3, 6 ]
+  end
+  
   test "formats a list of maps" do
     output = convert_to_array([
       %{name: "John Barry", zage: 27, foo: "bar"},
       %{name: "Mary J Blige", zage: 28, foo: "bar"}
-    ], [:zage, :name]) |> tableize
+    ], [:zage, :name]) |> stringify |> tableize
     
     assert output == """
     zage | name        
