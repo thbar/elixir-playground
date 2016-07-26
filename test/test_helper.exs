f571916473216cf647406f344637efcd712968a4
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-07-26 21:50:38 +0200

    Allow me to focus when working

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,6 @@
 ExUnit.start
-ExUnit.configure exclude: :pending, trace: true
+if System.get_env("FOCUS") == "1" do
+  ExUnit.configure(exclude: [:test, :pending], trace: true, include: :focus)
+else
+  ExUnit.configure exclude: :pending, trace: true
+end
