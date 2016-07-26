decfc49dce57e07c82615907b41e8723ec37cfa2
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-07-26 22:08:31 +0200

    Add a simple koan around basic message passing

---
+++ test/concurrency_test.exs
@@ -0,0 +1,25 @@
+defmodule ConcurrencyTest do
+  use ExUnit.Case, async: true
+  
+  defmodule Computator do
+    def double do
+      receive do
+        {sender, msg} ->
+          send sender, { :ok, 2 * msg }
+      end
+    end
+  end
+  
+  # @tag :focus
+  test "spawns a process that passes back a message" do
+    # run it in a separate process
+    pid = spawn(Computator, :double, [])
+    # send a message with the number we want to double
+    send pid, {self, 10}
+    # wait for the spawned process to send something back to us
+    receive do
+      {:ok, outcome} ->
+        assert 20 == outcome
+    end
+  end
+end
