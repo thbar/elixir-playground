17658f427ff4a8e4d92b2feeea34466edd0ba2de
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-07-26 22:08:17 +0200

    Add apex

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Playground do
       app: :playground,
       version: "1.0.0",
       deps: [
+        {:apex, "~> 0.4.0", only: [:dev, :test]},
         {:dogma, "~> 0.0", only: :dev},
         {:mix_test_watch, "~> 0.2", only: :dev}
       ]
