9f798dbd501950d751a0a5f6f8062168af13ab5e
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-09-19 20:11:42 +0200

    Unclutter the output when FOCUS=1

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 ExUnit.start
 if System.get_env("FOCUS") == "1" do
-  ExUnit.configure(exclude: [:test, :pending], trace: true, include: :focus)
+  # When focusing, I use trace false to unclutter the output from non-focused tests
+  ExUnit.configure(exclude: [:test, :pending], trace: false, include: :focus)
 else
   ExUnit.configure exclude: :pending, trace: true
 end
