6c93c0aafbfea646ede58cac347b857d58ea2390
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-11-06 18:38:34 +0100

    Update dependencies to latest

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule Playground do
       app: :playground,
       version: "1.0.0",
       deps: [
-        {:apex, "~> 0.4.0", only: [:dev, :test]},
-        {:dogma, "~> 0.0", only: :dev},
-        {:mix_test_watch, "~> 0.2", only: :dev}
+        {:apex, "~> 1.1.0", only: [:dev, :test]},
+        {:dogma, "~> 0.1.15", only: :dev},
+        {:mix_test_watch, "~> 0.5", only: :dev}
       ]
     ]
   end
