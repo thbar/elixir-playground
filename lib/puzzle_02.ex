8033c1f1de283e0e7bfff24e8992bb22e15ffdeb
Thibaut Barrère <thibaut.barrere@gmail.com>
2018-12-02 23:39:42 +0100

    Save shorter version

--- lib/puzzle_02.ex
+++ lib/puzzle_02.ex
@@ -1,29 +1,24 @@
 defmodule AdventOfCode2018.Puzzle02 do
-  def compute(input) do
-    data = input
+  def count_letters(input) do
+    input
     |> String.graphemes
     |> Enum.group_by(&(&1))
     |> Map.values
     |> Enum.map(&(Enum.count(&1)))
-
-    {
-      Enum.member?(data, 2),
-      Enum.member?(data, 3)
-    }
   end
   
   def solve(input) do
     {a, b} = input
     |> String.trim_trailing
     |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&compute(&1))
-    |> Enum.reduce({0, 0}, fn({two_count, three_count}, {two_total, three_total}) -> 
-      two_total = if two_count do two_total + 1 else two_total end
-      three_total = if three_count do three_total + 1 else three_total end
-      {two_total, three_total}
+    |> Enum.map(&String.trim(&1))
+    |> Enum.reduce({0, 0}, fn(input, {a, b}) ->
+      letters_count = count_letters(input)
+      {
+        a + if Enum.member?(letters_count, 2) do 1 else 0 end,
+        b + if Enum.member?(letters_count, 3) do 1 else 0 end
+      }
     end)
-    
     a * b
   end
 end
