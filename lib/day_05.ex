6f726c7a345b468a4c9477a80c565001e0c5199b
Thibaut Barrère <thibaut.barrere@gmail.com>
2018-12-05 09:11:56 +0100

    Save slow solution for day 05 puzzle 02

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -19,3 +19,25 @@ defmodule Day0501 do
     end
   end
 end
+
+defmodule Day0502 do
+
+  def length_without(unit_type, input) do
+    IO.puts "Processing unit type #{unit_type}"
+    polymer = input
+    |> String.replace(<<unit_type>>, "")
+    |> String.replace(<<unit_type + ?a - ?A>>, "")
+    |> Day0501.solve()
+    |> String.length
+  end
+
+  def solve(input) do
+    {_troubling_unit, shortest_polymer_size} = ?A..?Z
+    |> Enum.map(&({&1, length_without(&1, input)}))
+    |> Map.new
+    |> Enum.sort_by(fn({k,v}) -> v end)
+    |> List.first
+    
+    shortest_polymer_size
+  end
+end
