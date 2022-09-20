d02b496c69fc0a57004c8ac41ae7331201574bee
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-09-20 18:48:10 +0200

    Update stack

--- mix.exs
+++ mix.exs
@@ -4,12 +4,7 @@ defmodule Playground do
   def project do
     [
       app: :playground,
-      version: "1.0.0",
-      deps: [
-        {:apex, "~> 1.1.0", only: [:dev, :test]},
-        {:dogma, "~> 0.1.15", only: :dev},
-        {:mix_test_watch, "~> 0.5", only: :dev}
-      ]
+      version: "1.0.0"
     ]
   end
 end
